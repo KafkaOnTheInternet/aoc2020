ff041fc25eed7c9a7c502c55913149decee6ef57
KafkaOnTheInternet <arjun_shanewarne@yahoo.com>
2020-12-06 16:50:18 +0530

    Day 6 better version

--- 6.py
+++ 6.py
@@ -1,6 +1,29 @@
 import fileinput
 from collections import defaultdict
 
+uni = set()
+intr = set()
+
+ans1 = 0
+ans2 = 0
+
+for line in fileinput.input():
+    if len(line.strip()) == 0:
+        ans1 += len(uni)
+        ans2 += len(intr)
+        uni = set()
+        intr = set()
+    elif len(uni) == 0:
+        uni = set(line.strip())
+        intr = set(line.strip())
+    else:
+        uni = uni | set(line.strip())
+        intr = intr & set(line.strip())
+ans1 += len(uni)
+ans2 += len(intr)
+
+print(f'p1: {ans1}')
+print(f'p2: {ans2}')
 
 '''
 ans = 0
@@ -16,7 +39,7 @@ ans += len(S)
 print(ans)
 '''
 
-
+'''
 n = 0
 ans = 0
 S = defaultdict(int)
@@ -34,4 +57,4 @@ for line in fileinput.input():
 for k in S.keys():
     if S[k] == n: ans += 1
 print(ans)
-
+'''
